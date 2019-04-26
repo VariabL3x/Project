ccbc9707f2095183c826409a6c15c55723222754
kok huai <kokhuai_liew@hotmail.com>
2019-04-26 11:42:15 +0800

    WIP

---
+++ src/containers/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class NavBar extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
+}
+
