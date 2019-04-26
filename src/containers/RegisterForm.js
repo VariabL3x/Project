b47f7e99c3234ce795b02f9c1e2af31ef9a98107
kok huai <kokhuai_liew@hotmail.com>
2019-04-26 18:48:25 +0800

    WIP

--- src/containers/RegisterForm.js
+++ src/containers/RegisterForm.js
@@ -6,24 +6,41 @@ export default class RegisterForm extends React.Component{
         super(props)
 
         this.state = {
-
+            username:"",
+            email:"",
+            password:"",
+            confirm_password:""
         }
     }
+    handleChange = (e) =>{
+        let target = e.target;
+        let value = target.value;
+        let name = target.name;
 
+        this.setState({
+            [name]:value
+        })
+    }
+
+    handleSubmit = (e) => {
+        if(this.state.password === this.state.confirm_password){
+            
+        }
+    }
     render(){
         return(
             <form className="register-form-box">
                 <label>Username</label>
-                <input type="text"/>
+                <input type="text" name="useranme" onChange={this.handleChange}/>
 
                 <label>Email</label>
-                <input type="email"/>
+                <input type="email" name="email" onChange={this.handleChange}/>
 
                 <label>Password</label>
-                <input type="password"/>
+                <input type="password" name="password" onChange={this.handleChange}/>
 
                 <label>Confirm Password</label>
-                <input type="password"/>
+                <input type="password" name="confirm_password" onChange={this.handleChange}/>
 
                 <input type="submit" value="Register"/>
 
