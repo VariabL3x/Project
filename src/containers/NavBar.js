a2cb0724788b2f01f50c87208a605982005d1ff6
kok huai <kokhuai_liew@hotmail.com>
2019-04-26 13:54:12 +0800

    navbar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,20 +1,42 @@
 import React from 'react'
-
+import { Layout,Header,Navigation,Content } from 'react-mdl'
 export default class NavBar extends React.Component{
     constructor(props){
         super(props)
 
         this.state = {
-
+            isLogin:false
         }
     }
 
     render(){
-        return(
-            <>
-            <h1>Hello from navbar</h1>
-            </>
-        )
+        return this.state.isLogin?( 
+            <div style={{height: '80px', position: 'relative'}}>
+                <Layout fixedHeader>
+                    <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>is log in</strong></span>}>
+                        <Navigation>
+                            <a href="/"><strong>Home</strong></a>
+                            <a href="/"><strong>Post</strong></a>
+                            <a href="/"><strong>Account</strong></a>
+                            <a href="/"><strong>Logout</strong></a>
+                        </Navigation>
+                    </Header>
+                    <Content />
+                </Layout>
+            </div>
+        ) : <div style={{height: '100vh', position: 'relative'}}>
+                <Layout fixedHeader>
+                    <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>not logged in</strong></span>}>
+                        <Navigation>
+                            <a href="/"><strong>Home</strong></a>
+                            <a href="/login"><strong>Login</strong></a>
+                            <a href="/register"><strong>Register</strong></a>
+                        </Navigation>
+                    </Header>
+                    <Content />
+                </Layout>
+            </div>
+        
     }
 }
 
